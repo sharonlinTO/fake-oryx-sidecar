af181c98908de1a77ecfdb86b82afa92f0c7072c
Sharon Lin <t-sharonlin@microsoft.com>
2021-08-30 12:09:53 -0400

    commit

--- app.py
+++ app.py
@@ -4,31 +4,35 @@ import subprocess
 
 app = Flask(__name__)
 
+@app.route('/', methods = ['GET'])
+def send_success():
+    return Response('I am fake Oryx :)', 200)
+
 # NOTE: file must be called main.go
-@app.route('/', methods = ['POST'])
+@app.route('/build', methods = ['POST'])
 def compile_go():
 
     # Only accept POST requests
     if request.method != "POST":
-        return Response('Only POST requests are supported.', 405)
+        return Response(f'Only POST requests are supported. {request.form} ', 405)
     
     # Only support Golang for now
-    framework = request.form.get("framework")
-    if framework != "GO":
-        return Response(f'Only Go framework is supported at this time.', 400)
+    platform = request.form.get("platform")
+    if platform != "go":
+        return Response(f'Only Go platform is supported at this time. Submitted platform: {request.form}', 400)
 
     # File should be in repository folder
     fileLoc = "/home/site/repository/main.go"
     if not os.path.isfile(fileLoc):
-        return Response(f'File not found. Looking for: {fileLoc}', 404)
+        return Response(f'File not found. Looking for: {fileLoc} {request.form} ', 404)
 
     # Place file in /home/site/wwwroot/
     returnCode = subprocess.call(["go", "build", "-o", "/home/site/wwwroot/main", fileLoc], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     if returnCode == 0:
-        return Response(f'Build complete. Binary located in /home/site/wwwroot/', 200)
+        return Response(f'Build complete. Binary located in /home/site/wwwroot/ {request.form} ', 200)
     else:
-        return Response('Build Failed.', 400)
+        return Response(f'Build Failed. {request.form} ', 400)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
