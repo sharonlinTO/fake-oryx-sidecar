2d2d03de16fbbc12a2a8f49cbc6ea3e698b95585
Sharon Lin <t-sharonlin@microsoft.com>
2021-09-01 18:10:15 -0400

    Fix endpoints and event handling

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, render_template
 import os
 import subprocess
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+@app.route('/')
 def send_success():
-    return Response('I am fake Oryx :)', 200)
+    return Response('I am fake Oryx :)\n', 200)
+
+@app.route('/Default.cshtml')
+def load_html():
+    return Response("Kestrel Running", 200)
 
 # NOTE: file must be called main.go
 @app.route('/build', methods = ['POST'])
@@ -14,7 +18,7 @@ def compile_go():
 
     # Only accept POST requests
     if request.method != "POST":
-        return Response(f'Only POST requests are supported. {request.form} ', 405)
+        return Response(f'Only POST requests are supported. \n {request.form} ', 405)
     
     # Only support Golang for now
     platform = request.form.get("platform")
@@ -24,15 +28,15 @@ def compile_go():
     # File should be in repository folder
     fileLoc = "/home/site/repository/main.go"
     if not os.path.isfile(fileLoc):
-        return Response(f'File not found. Looking for: {fileLoc} {request.form} ', 404)
+        return Response(f'File not found. Looking for: {fileLoc} \n {request.form} ', 404)
 
     # Place file in /home/site/wwwroot/
     returnCode = subprocess.call(["go", "build", "-o", "/home/site/wwwroot/main", fileLoc], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     if returnCode == 0:
-        return Response(f'Build complete. Binary located in /home/site/wwwroot/ {request.form} ', 200)
+        return Response(f'Build complete. Binary located in /home/site/wwwroot/ \n {request.form} ', 200)
     else:
-        return Response(f'Build Failed. {request.form} ', 400)
+        return Response(f'Build Failed. \n {request.form} ', 400)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
